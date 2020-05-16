9defe32c1a952ceb73dbaaa5c206842cac49fd4a
John Medlock <42301475+johnMedlockDev@users.noreply.github.com>
2020-05-16 11:06:38 -0500

    init

---
+++ src/main/java/com/virtualpairprogrammers/Main.java
@@ -0,0 +1,14 @@
+package com.virtualpairprogrammers;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Main {
+    public static void main(String[] args) {
+
+        List<Double> inputData = new ArrayList<>();
+        inputData.add(35.5);
+        inputData.add(32345.5);
+        inputData.add(224.53);
+    }
+}
