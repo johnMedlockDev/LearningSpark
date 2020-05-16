855d6e1310bc1d49cbe0561126ad67ad231b564a
John Medlock <42301475+johnMedlockDev@users.noreply.github.com>
2020-05-16 11:30:43 -0500

    it works

--- src/main/java/com/virtualpairprogrammers/Main.java
+++ src/main/java/com/virtualpairprogrammers/Main.java
@@ -1,5 +1,11 @@
 package com.virtualpairprogrammers;
 
+import org.apache.log4j.Level;
+import org.apache.log4j.Logger;
+import org.apache.spark.SparkConf;
+import org.apache.spark.api.java.JavaRDD;
+import org.apache.spark.api.java.JavaSparkContext;
+
 import java.util.ArrayList;
 import java.util.List;
 
@@ -10,5 +16,19 @@ public class Main {
         inputData.add(35.5);
         inputData.add(32345.5);
         inputData.add(224.53);
+
+//        remove boilerplate logging
+        Logger.getLogger("org.apache").setLevel(Level.WARN);
+
+//        local context with max threads available
+        SparkConf conf = new SparkConf().setAppName("Spark").setMaster("local[*]");
+        JavaSparkContext sc = new JavaSparkContext(conf);
+
+//        Wrapper class
+        JavaRDD<Double> myRDD = sc.parallelize(inputData);
+
+//        closes JavaSparkContext
+        sc.close();
+
     }
 }
